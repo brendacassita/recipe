b105d3ac101ae44712b26ee23e773b960f3cc8e8
Brenda <brendacassita@gmail.com>
2022-06-09 18:22:56 -0600

    card css

--- src/components/Popular.js
+++ src/components/Popular.js
@@ -24,11 +24,16 @@ const Popular = () => {
 
   return (
     <div>
-      {popular.map((recipe) => {
-        return(
+        
           <Wrapper>
             <h3>Popular Picks</h3>
-            <Splide>
+            <Splide options={{
+              perPage: 5,
+              arrows: false,
+              pagination: false,
+              drag: 'free',
+              gap: '5rem'
+            }}>
             {popular.map((recipe)=>{
               return(
                 <SplideSlide>
@@ -42,24 +47,44 @@ const Popular = () => {
             })}
             </Splide>
             </Wrapper>
-
-        )
-        
-      })}
     </div>
   )
 }
 
 const Wrapper = styled.div`
-  margin: 4rem 0rem;`;
+  margin: 2rem 0rem;`;
 
 const Card = styled.div`
-  min-height: 25rem;
-  border-radius: 2rem;
+  min-height: 20rem;
+  border-radius: 1.5rem;
   overflow: hidden;
+  position: relative;
 
   img{
     border-radius: 2rem;
+    position: absolute;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+
+  }
+
+  p{
+    position: absolute;
+    z-index: 10;
+    left: 50%;
+    bottom: 0%;
+    transform: translate(-50%, 0%);
+    color: white;
+    width: 100%;
+    text-align: center;
+    font-weight: 600;
+    font-size: 1rem;
+    height: 40%;
+    display: flex;
+    justify-content: center;
+    align-items: center
 
   }`
 
